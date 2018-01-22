34205300dc51f535ba8eadbe5fa3174f36bdf58b
Fredrik Olsson <froa16@student.bth.se>
2018-01-22 14:40:39 +0100

    Simple song opener

--- Stuffa/MainWindow.xaml.cs
+++ Stuffa/MainWindow.xaml.cs
@@ -25,7 +25,7 @@ namespace Stuffa
     public partial class MainWindow : Window
     {
         bool isPlaying = false;
-
+        Music[] songs = new Music[3];
         // Process all files in the directory passed in, recurse on any directories 
         // that are found, and process the files they contain.
         public static void ProcessDirectory(string targetDirectory, ListBox list)
@@ -45,7 +45,7 @@ namespace Stuffa
         public static void ProcessFile(string path, ListBox list)
         {
             //gets the file name
-            list.Items.Add(System.IO.Path.GetFileNameWithoutExtension(path));
+            list.Items.Add(path);
            // Console.WriteLine("Processed file '{0}'.", path);
         }
 
@@ -53,7 +53,7 @@ namespace Stuffa
         {
             InitializeComponent();
 
-            ProcessDirectory("E:\\grupp.proj\\YTExampleMusik", list);
+            ProcessDirectory("C:\\Users\\Fredrik\\source\\repos\\stuffa\\Musik\\", list);
 
             progresBar.Value = 0.5;
          
@@ -62,12 +62,17 @@ namespace Stuffa
         private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             string path = list.SelectedItem.ToString();
-            path = "E:\\grupp.proj\\YTExampleMusik\\" + path + ".mp3";
-
+            // Denna funkar bara efter att man har lagt till  en ny låt - därför utkommenterad
+            //textBox.Text = "Path: " + songs[0].path + " Title: " + songs[0].name;
             TagLib.File tagFile = TagLib.File.Create(path);
             string artist = tagFile.Tag.JoinedPerformers;
+            string album = tagFile.Tag.JoinedAlbumArtists;
+            string songName = tagFile.Tag.Title;
+            var length = tagFile.Properties.Duration;
 
-            text.Text = path + "\n\nmusik grupp: " + artist;
+            label.Content = length;
+
+            text.Text = path + "\n\nmusik grupp: " + artist + " Album: " + album + " Title: " + songName;
 
             player.Source = new Uri(path, UriKind.RelativeOrAbsolute);
 
@@ -98,6 +103,44 @@ namespace Stuffa
             
 
             
+        }
+
+        private void comboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void MenuItem_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+ 
+        private void open_song_click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+
+            dlg.DefaultExt = ".mp3";
+            dlg.Filter = "MP3 Files (*.mp3)|*.mp3| FLAC Files (*.flac)|*.flac";
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            Nullable<bool> result = dlg.ShowDialog();
+
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                string filename = dlg.FileName;
+                list.Items.Add(filename);
+                textBox1.Text = filename;
+
+                songs[0] = new Music(filename, System.IO.Path.GetFileNameWithoutExtension(filename));
+            }
+        }
+
+        private void progresBar_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+
         }
     }
 }
---
+++ Stuffa/Music.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stuffa
+{
+    class Music
+    {
+        public string path;
+        public string name;
+
+        public Music()
+        {
+
+        }
+        public Music(string path, string name)
+        {
+            this.path = path + name + ".mp3";
+            this.name = name;
+        }
+    }
+}
+
