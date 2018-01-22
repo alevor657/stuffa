91031e97b51e0d142e018894440e85f6e9dd1227
Fredrik Olsson <froa16@student.bth.se>
2018-01-22 12:13:03 +0100

    Fixes

--- Stuffa/MainWindow.xaml.cs
+++ Stuffa/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace Stuffa
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
--- Stuffa/Properties/AssemblyInfo.cs
+++ Stuffa/Properties/AssemblyInfo.cs
@@ -7,11 +7,11 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WpfApp1")]
+[assembly: AssemblyTitle("Stuffa")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Hewlett-Packard")]
-[assembly: AssemblyProduct("WpfApp1")]
+[assembly: AssemblyProduct("Stuffa")]
 [assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
